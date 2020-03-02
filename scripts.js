b33aaf4e9c0d512770c2dbf602bb106169d167b8
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-03-02 15:42:40 +0300

    created prompt and alert functions using JQuery

--- scripts.js
+++ scripts.js
@@ -76,3 +76,12 @@ function Total() {
 
 }
 
+//JQuery alert
+$(document).ready(function(){
+  $("#deliver").click(function(){
+  prompt("State the location for delivery" )
+  });
+  $("#no").click(function(){
+  alert("Thank you for shopping with us and welcome again")
+  });
+})
