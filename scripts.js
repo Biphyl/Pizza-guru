440aa57e81490e2189287c753e55de93ffce1c48
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-03-02 15:22:59 +0300

    created function for size, crust and toppings

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+// number of size
+function size(){
+  var size =parseInt(document.getElementById("size")).value;
+  return parseInt(size);
+}
+
+// number of crust
+function crust(){
+  var crust =parseInt(document.getElementById("crust")).value;
+  return parseInt(crust)
+}
+
+function getNumber() {
+  var getNumber = parseInt(document.getElementById("getnumber")).value;
+  return parseInt(getNumber);
+}
+// number of topping
+function topping(){
+  var topping = document.getElementById("topping").value;
+  return parseInt(topping)
+}
